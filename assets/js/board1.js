970249fe69b171704134f805b7856c85a20316d2
sioge <mjg20000@gmail.com>
2021-03-15 15:15:02 +0900

    get local data and create table

--- assets/js/board1.js
+++ assets/js/board1.js
@@ -10,6 +10,23 @@ Board1.prototype.document = function(){
     $("#panel_left").load("/assets/layout/left.html");
     $("#footer").load("/assets/layout/footer.html");
 
+    self.createTable();
+}
 
-
+Board1.prototype.createTable = function(){
+    let self = this;
+    
+    $.getJSON('/testData/board1.json', function(data) {
+        $.each(data, function(index, item){
+            $("#panel_contents tbody").append(
+                "<tr>" +
+                    "<td>" + item.idx + "</td>" +
+                    "<td>" + item.id + "</td>" +
+                    "<td>" + item.subject + "</td>" +
+                    "<td>" + item.contents + "</td>" +
+                    "<td>" + item.regdate + "</td>" +
+                "</tr>"
+            );
+        })
+    })
 }
