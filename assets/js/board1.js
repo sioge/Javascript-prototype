88f57150b6e5257f8c12d1794ffdc4d751c99699
sioge <mjg20000@gmail.com>
2021-03-29 14:09:57 +0900

    input form

--- assets/js/board1.js
+++ assets/js/board1.js
@@ -1,5 +1,7 @@
 const Board1 = function(){
 
+
+    this.count = 5;
     this.document();
 }
 
@@ -11,6 +13,7 @@ Board1.prototype.document = function(){
     $("#footer").load("/assets/layout/footer.html");
 
     self.createTable();
+    self.clickEvent();
 }
 
 Board1.prototype.createTable = function(){
@@ -31,6 +34,7 @@ Board1.prototype.createTable = function(){
                 html +=     "<td>" + item.subject + "</td>";
                 html +=     "<td>" + item.contents + "</td>";
                 html +=     "<td>" + item.regdate + "</td>";
+                html +=     "<td><button type='button' name='delete' value='" +  item.idx + "' >X</button></td>";
                 html += "</tr>";
                 
                 $("#panel_contents tbody").append(html);
@@ -45,4 +49,48 @@ Board1.prototype.createTable = function(){
 }
 Board1.prototype.clickEvent = function(){
     let self = this;
+
+    $("button[name=test]").unbind().click(function(){
+        let formData = $("form[name=form_test]").serializeObject();
+        let date = $.getToday();
+        
+        if(self.checkEvent("form_test") === false) return false;
+    
+        let html = "";
+        html += "<tr>";
+        html +=     "<td>" + self.count + "</td>";
+        html +=     "<td>" + formData.id + "</td>";
+        html +=     "<td>" + formData.subject + "</td>";
+        html +=     "<td>" + formData.contents + "</td>";
+        html +=     "<td>" + date + "</td>";
+        html +=     "<td><button type='button' name='delete' value='" + self.count + "'>X</button></td>";
+        html += "</tr>";
+        
+        $("#panel_contents tbody").append(html);
+
+        $("form[name=form_test]")[0].reset();
+        self.count++;
+        self.clickEvent();
+    })
+
+    $("button[name=delete]").unbind().click(function(){
+        let target = $(this).val();
+
+        $("table tbody").find("button[value=" + target + "]").parents("tr").remove();
+    })
+}
+Board1.prototype.checkEvent = function(type){
+    let self = this;
+
+    let input = $("form[name=" + type + "] input");
+    let bool = true;
+    $.each(input, function(index, item){
+        if(item.value == ""){
+            input[index].focus();
+            bool = false;
+            return false;
+        }
+    })
+
+    return bool;
 }
--- assets/js/function.js
+++ assets/js/function.js
@@ -24,3 +24,11 @@ $.fn.pagination = function(page, dataPerPage, total){
   totalPage = Math.ceil(total / dataPerPage);
   
 }
+$.getToday = function(){
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = ("0" + (1 + date.getMonth())).slice(-2);
+  let day = ("0" + date.getDate()).slice(-2);
+
+  return year + "-" + month + "-" + day;
+}
